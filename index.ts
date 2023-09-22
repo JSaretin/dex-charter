45b16f8a7c273a6265e6eb2ebe953a8fcfa4ddfc
JSaretin <jsaretin@gmail.com>
2023-09-22 23:36:18 +0100

    check token balance before trading

--- index.ts
+++ index.ts
@@ -42,6 +42,7 @@ async function generateTradeData(
   return data;
 }
 
+// deploy transaction to the blockchain
 async function executeTransaction(
   data: string,
   to: string,
@@ -70,16 +71,24 @@ async function trade(
   receiveToken: string,
   amount: number
 ) {
+  const weiAmount = w3.utils.toWei(amount.toString(), "ether");
   const wallet = w3.eth.accounts.privateKeyToAccount(privateKey);
 
-  const data = await generateTradeData(
-    wallet.address,
-    baseToken,
-    receiveToken,
-    amount
-  );
-
   const contract = new w3.eth.Contract(ERC20_ABI, baseToken);
+  const baseBalance = await (contract.methods.balanceOf as any)(
+    wallet.address
+  ).call();
+
+  if (baseBalance < weiAmount) {
+    console.log(
+      `Not enough balance to trade\nBalance: ${w3.utils.fromWei(
+        baseBalance,
+        "ether"
+      )} BNB\nRequired: ${amount} ETH`
+    );
+    return;
+  }
+
   const allowance = await (contract.methods.allowance as any)(
     wallet.address,
     DEX_ROUTER_ADDR
@@ -88,7 +97,7 @@ async function trade(
   // if amount to trade is greater the the allowance, approve totalSupply to
   // the DEX contract (this will save us gas in the long run since these are just
   // dummy accounts that we will not be using to hold for long)
-  if (allowance < w3.utils.toWei(amount.toString(), "ether")) {
+  if (allowance < weiAmount) {
     const totalSupply = await (contract.methods.totalSupply as any)().call();
     const approveData = (contract.methods.approve as any)(
       DEX_ROUTER_ADDR,
@@ -101,6 +110,13 @@ async function trade(
     );
     console.log(approveTransaction);
   }
+
+  const data = await generateTradeData(
+    wallet.address,
+    baseToken,
+    receiveToken,
+    amount
+  );
 
   const transaction = await executeTransaction(
     data,
@@ -141,13 +157,20 @@ async function generateWallet(
 
 const w = await generateWallet(SEED_PHRASE);
 // console.log(w)
-
-sendToken(
-  "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+const t = await trade(
   w.privateKey,
-  "0x5A568d8280d499083F6b1BF1D4B546ac24486948",
-  0
+  "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+  "0xEd95A3Ea53457b93A4439C76803F5B60218D253f",
+  0.0001
 );
+console.log(t);
+
+// sendToken(
+//   "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+//   w.privateKey,
+//   "0x5A568d8280d499083F6b1BF1D4B546ac24486948",
+//   0
+// );
 
 // for (let i = 0; i < 10; i++) {
 //   const w = await generateWallet(SEED_PHRASE, i);
