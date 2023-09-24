6401475086fee4d839fc156a79d8d08db37faa36
JSaretin <jsaretin@gmail.com>
2023-09-24 17:04:36 +0100

    added utils

---
+++ utils.ts
@@ -0,0 +1,33 @@
+import { HDNodeWallet } from "ethers";
+import Web3 from "web3";
+import { CreatedWallet } from "./structure";
+import { BunFile } from "bun";
+
+export function toEther(amount: any) {
+  const result = Web3.utils.fromWei(amount.toString(), "ether");
+  return Number(result);
+}
+
+export function toWei(amount: number) {
+  return Web3.utils.toWei(amount.toString(), "ether");
+}
+
+export async function generateWallet(
+  seed: string,
+  index: number = 0
+): Promise<CreatedWallet> {
+  const oracle = HDNodeWallet.fromPhrase(seed) as HDNodeWallet;
+  const wallet = oracle.deriveChild(index);
+
+  const newWallet = {
+    privateKey: wallet.privateKey,
+    address: wallet.address,
+    index: wallet.index,
+  };
+  return newWallet;
+}
+
+// write data to disk file
+export async function writeData(path: BunFile, data: object) {
+  await Bun.write(path, JSON.stringify(data));
+}
